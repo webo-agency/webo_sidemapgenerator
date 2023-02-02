7637f22c7ab9b370f831b2650bb5f34c21cb0b91
mit2 <45004422+mit2pl@users.noreply.github.com>
2023-02-02 23:43:36 +0100

    finish module

--- src/Form/ConfigurationDataConfiguration.php
+++ src/Form/ConfigurationDataConfiguration.php
@@ -10,6 +10,7 @@ use PrestaShop\PrestaShop\Core\ConfigurationInterface;
 class configurationDataConfiguration implements DataConfigurationInterface
 {
     public const WEBO_MODULE_ACTIVATION = 'WEBO_FILTERSITEMAP_ACTIVE';
+    public const WEBO_INDEXED_MODULE = 'WEBO_FILTERSITEMAP_SITEMAPINDEX';
     /**
      * @var ConfigurationInterface
      */
@@ -29,16 +30,21 @@ class configurationDataConfiguration implements DataConfigurationInterface
     {
 
         $this->configuration->set(static::WEBO_MODULE_ACTIVATION, $configuration['active_module']);
+        $this->configuration->set(static::WEBO_INDEXED_MODULE, $configuration['check_if_indexed']);
         return [];
     }
 
-    public function getConfiguration()
+    public function getConfiguration():array
     {
         $return = [];
 
         if($configActiveModule = $this->configuration->get(static::WEBO_MODULE_ACTIVATION))
         {
             $return['active_module'] = $configActiveModule;
+        }
+        if($configIndexedModule = $this->configuration->get(static::WEBO_INDEXED_MODULE))
+        {
+            $return['check_if_indexed'] = $configIndexedModule;
         }
         return $return;
     }
--- src/Form/ConfigurationType.php
+++ src/Form/ConfigurationType.php
@@ -24,6 +24,15 @@ class ConfigurationType extends TranslatorAwareType
                 'required' => false,
                 'label' => 'Enable module',
                 'help' => 'Confirm whether the module is to be active',
+            ])
+            ->add('check_if_indexed', SwitchType::class, [
+                'choices' => [
+                    'Disable' => 0,
+                    'Enable' => 1,
+                ],
+                'required' => false,
+                'label' => 'Module must be indexed',
+                'help' => 'If module is Google Indexed'
             ]);
     }
 
